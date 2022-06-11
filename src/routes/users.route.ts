fdb246ee777b3e7fb002b3cbd5187832e6423f23
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-11 19:14:33 +0800

    added middlewares to users routes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,6 +3,9 @@ import UsersController from '@controllers/users.controller';
 import { CreateUserDto } from '@dtos/users.dto';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
+import authMiddleware from '@/middlewares/auth.middleware';
+import permisssionMiddleware from '@/middlewares/permission.middleware';
+import { UserRoleType } from '@/interfaces/userRoleType.interface';
 
 class UsersRoute implements Routes {
   public path = '/users';
@@ -14,11 +17,11 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
-    this.router.get(`${this.path}/:id`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id`, this.usersController.deleteUser);
+    this.router.get(`${this.path}`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN,UserRoleType.EDITOR]), this.usersController.getUsers);
+    this.router.get(`${this.path}/:id`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN,UserRoleType.EDITOR]),this.usersController.getUserById);
+    this.router.post(`${this.path}`, authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put(`${this.path}/:id`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    this.router.delete(`${this.path}/:id`, authMiddleware,permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), this.usersController.deleteUser);
   }
 }
 
