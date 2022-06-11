954795a16c7091e3e33c0817458c7325b6797eca
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-11 19:12:07 +0800

    added fields to user interface

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,6 +1,17 @@
+import { CreateUserDto } from '@/dtos/users.dto';
+import { UserRoleType } from './userRoleType.interface';
+import { UserStatus } from './userStatus.interface';
+
 export interface User {
   _id: string;
   email: string;
   password: string;
+  name: string;
+  role: UserRoleType;
+  status: UserStatus;
   date_joined: Date;
+}
+
+export interface SuperAdminSignup {
+  signup(userData: CreateUserDto):Promise<User>;
 }
