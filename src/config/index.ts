cc37fa2b0b9b91619e6c12d6baa92a3bc8c3bc4c
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-13 00:48:02 +0800

    added env var for superadmin

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, SUPER_ADMIN_EMAIL, SUPER_ADMIN_PWD } = process.env;
+export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, SUPER_ADMIN_EMAIL, SUPER_ADMIN_PWD } =
+  process.env;
