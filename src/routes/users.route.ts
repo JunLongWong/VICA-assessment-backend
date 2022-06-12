0eef7751edd256857097e5b6eba794b2e580d40d
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-13 00:49:42 +0800

    fix linting errors

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,7 +5,7 @@ import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
 import authMiddleware from '@/middlewares/auth.middleware';
 import permisssionMiddleware from '@/middlewares/permission.middleware';
-import { UserRoleType } from '@/interfaces/userRoleType.interface';
+import { UserRoleType } from '@/interfaces/userRoleType.enum';
 
 class UsersRoute implements Routes {
   public path = '/users';
@@ -17,11 +17,38 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN,UserRoleType.EDITOR]), this.usersController.getUsers);
-    this.router.get(`${this.path}/:id`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN,UserRoleType.EDITOR]),this.usersController.getUserById);
-    this.router.post(`${this.path}`, authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id`,authMiddleware, permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id`, authMiddleware,permisssionMiddleware([UserRoleType.SUPER_ADMIN,UserRoleType.ADMIN]), this.usersController.deleteUser);
+    this.router.get(
+      `${this.path}`,
+      authMiddleware,
+      permisssionMiddleware([UserRoleType.SUPER_ADMIN, UserRoleType.ADMIN, UserRoleType.EDITOR]),
+      this.usersController.getUsers,
+    );
+    this.router.get(
+      `${this.path}/:id`,
+      authMiddleware,
+      permisssionMiddleware([UserRoleType.SUPER_ADMIN, UserRoleType.ADMIN, UserRoleType.EDITOR]),
+      this.usersController.getUserById,
+    );
+    this.router.post(
+      `${this.path}`,
+      authMiddleware,
+      permisssionMiddleware([UserRoleType.SUPER_ADMIN, UserRoleType.ADMIN]),
+      validationMiddleware(CreateUserDto, 'body'),
+      this.usersController.createUser,
+    );
+    this.router.put(
+      `${this.path}/:id`,
+      authMiddleware,
+      permisssionMiddleware([UserRoleType.SUPER_ADMIN, UserRoleType.ADMIN]),
+      validationMiddleware(CreateUserDto, 'body', true),
+      this.usersController.updateUser,
+    );
+    this.router.delete(
+      `${this.path}/:id`,
+      authMiddleware,
+      permisssionMiddleware([UserRoleType.SUPER_ADMIN, UserRoleType.ADMIN]),
+      this.usersController.deleteUser,
+    );
   }
 }
 
