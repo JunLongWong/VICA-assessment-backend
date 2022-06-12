0ba667810e77d68f9a548cfab8b63f8a19d28669
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-12 22:18:13 +0800

    added book schema for Book Collection

---
+++ src/models/book.model.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from 'mongoose';
+import { Book } from '@/interfaces/book.interface';
+
+const bookSchema = new Schema<Book>({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  published_year: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+// creation of book model
+const bookModel = model<Book>('Book', bookSchema);
+
+export { bookModel };
