32712033ffc305cfb23611ca664dde2f43c99048
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-13 00:55:17 +0800

    minor fix on permissions middleware

--- src/middlewares/permission.middleware.ts
+++ src/middlewares/permission.middleware.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
-import { UserRoleType } from '@/interfaces/userRoleType.interface';
+import { UserRoleType } from '@/interfaces/userRoleType.enum';
 
 const permisssionMiddleware = (roles: UserRoleType[]) => (req: Request, res: Response, next: NextFunction) => {
-    try {
+  try {
     // get user role
-    const role = req['role']
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>",role);
-    
+    const role = req['role'];
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', role);
+
     // check if user role is in roles
-    if(roles.indexOf(role) !== -1) {
-        next()
+    if (roles.indexOf(role) !== -1) {
+      next();
     } else {
-        next(new HttpException(401, 'You are unauthorized !'));
+      next(new HttpException(401, 'You are unauthorized !'));
     }
   } catch (error) {
     next(error);
