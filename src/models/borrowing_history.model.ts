166ca580b6e37b7a500bdaab5ac8770f193eb9cc
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-15 09:22:42 +0800

    added borrowing history schema

---
+++ src/models/borrowing_history.model.ts
@@ -0,0 +1,35 @@
+import { BookAvailability } from '@/interfaces/bookAvailability.enum';
+import { BookStatus } from '@/interfaces/bookStatus.enum';
+import { BorrowingHistory } from '@/interfaces/borrowingHistory.interface';
+import { model, Schema } from 'mongoose';
+
+const borrowingHistorySchema = new Schema<BorrowingHistory>({
+  user: {
+    type: Object,
+    required: true,
+  },
+  book: {
+    type: Object,
+    required: true,
+  },
+  date_borrowed: {
+    type: Date,
+    required: true,
+  },
+  date_returned: {
+    type: Date,
+  },
+  status: {
+    type: BookStatus,
+    required: true,
+  },
+  availability: {
+    type: BookAvailability,
+    required: true,
+  },
+});
+
+// creation of borrowingHistory model
+const borrowingHistoryModel = model<BorrowingHistory>('BorrowingHistory', borrowingHistorySchema);
+
+export { borrowingHistoryModel };
