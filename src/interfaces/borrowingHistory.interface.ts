732755087c59c3a427ee8bfc629aea33461e0a56
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-15 09:23:35 +0800

    added BorrowingHIstory interface

---
+++ src/interfaces/borrowingHistory.interface.ts
@@ -0,0 +1,12 @@
+import { BookAvailability } from "./bookAvailability.enum";
+import { BookStatus } from "./bookStatus.enum";
+
+export interface BorrowingHistory {
+  _id: string;
+  user: Object;
+  book: Object;
+  date_borrowed: Date;
+  date_returned: Date;
+  status: BookStatus;
+  availability: BookAvailability;
+}
