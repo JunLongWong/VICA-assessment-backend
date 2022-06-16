45ac7599d9d6cb0ff13c0847ccd6e9037736caa2
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-16 18:10:48 +0800

    added borrow history controller

---
+++ src/controllers/borrowHistory.controller.ts
@@ -0,0 +1,43 @@
+import { AddEditBorrowingHistoryDto } from '@/dtos/AddEditBorrowingHistory.dto';
+import { BorrowingHistory } from '@/interfaces/borrowingHistory.interface';
+import BorrowHistoryService from '@/services/borrowHistory.service';
+import { NextFunction, Request, Response } from 'express';
+
+class BorrowHistoryController {
+  public borrowHistoryService = new BorrowHistoryService();
+
+  public getBorrowHistoryById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const borrowHistoryId: string = req.params.id;
+      const findOneBorrowingHistoryData: BorrowingHistory = await this.borrowHistoryService.findBorrowHistoryById(borrowHistoryId);
+      res.status(200).json({ BorrowHistoryData: findOneBorrowingHistoryData, message: 'findOne' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public createBorrowHistory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const borrowHistoryData: AddEditBorrowingHistoryDto = req.body;
+      const createBorrowHistoryData: BorrowingHistory = await this.borrowHistoryService.createBorrowHistory(borrowHistoryData);
+
+      res.status(201).json({ BorrowingHistory: createBorrowHistoryData, message: 'Borrow history created successfully!' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateBorrowHistory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const borrowHistoryId: string = req.params.id;
+      const borrowHistoryData: AddEditBorrowingHistoryDto = req.body;
+      const updateBorrowingHistoryData: BorrowingHistory = await this.borrowHistoryService.updateBorrowHistory(borrowHistoryId, borrowHistoryData);
+
+      res.status(200).json({ BorrowingHistory: updateBorrowingHistoryData, message: 'Borrowing history updated successfully!' });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default BorrowHistoryController;
