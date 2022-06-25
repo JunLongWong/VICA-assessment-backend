776886a3ca96d896b97dbac2498572492572642e
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-26 02:56:31 +0800

    added borrowhistory routes

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,16 @@ import UsersRoute from '@routes/users.route';
 import BooksRoute from './routes/books.route';
 import validateEnv from '@utils/validateEnv';
 import UserService from './services/users.service';
+import BorrowHistoryRoute from './routes/borrowHistory.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new BooksRoute()], new UserService());
+const app = new App([
+    new IndexRoute(),
+    new UsersRoute(),
+    new AuthRoute(),
+    new BooksRoute(),
+    new BorrowHistoryRoute()
+], new UserService());
 
 app.listen();
